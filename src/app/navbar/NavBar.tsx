99ff3043f8082b288fa30449c14372a4044f48f0
lucasborba111 <lucasborgesborba@gmail.com>
2023-10-21 21:07:18 -0300

    finishing navbar cart and drop down

--- src/app/navbar/NavBar.tsx
+++ src/app/navbar/NavBar.tsx
@@ -1,7 +1,23 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '@/assets/logo.jpg'
-export default function NavBar() {
+import { redirect } from 'next/navigation'
+import { getCart } from '../lib/db/cart'
+import ShoppingCartButton from './ShoppingCartButton'
+
+async function searchProducts(formData: FormData) {
+  'use server'
+
+  const searchQuery = formData.get('searchQuery')?.toString()
+
+  if (searchQuery) {
+    redirect('/search?query=' + searchQuery)
+  }
+}
+
+export default async function NavBar() {
+  const cart = await getCart()
+
   return (
     <div className="bg-base-100">
       <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row">
@@ -10,6 +26,18 @@ export default function NavBar() {
             <Image src={logo} height={40} width={40} alt="ecommerce logo" />
             Ecommerce
           </Link>
+        </div>
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input
+                name="searchQuery"
+                placeholder="Search"
+                className="input input-bordered w-full min-w-[100px]"
+              />
+            </div>
+          </form>
+          <ShoppingCartButton cart={cart} />
         </div>
       </div>
     </div>
